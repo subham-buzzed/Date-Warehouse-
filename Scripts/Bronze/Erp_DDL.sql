23c01812c475759ac852eca75398ffc31b1c66b7
subham-buzzed <subhamworks24@gmail.com>
2026-03-31 10:30:47 +0530

    Create tables erp_loc_a101, erp_cust_az12, erp_px_cat_g1v2
    This script creates three tables: erp_loc_a101, erp_cust_az12, and erp_px_cat_g1v2 in the Bronze schema, dropping them if they already exist.

--- Scripts/Bronze/Erp_DDL.sql
+++ Scripts/Bronze/Erp_DDL.sql
@@ -1,1 +1,38 @@
+-- Create Erp_loc_a101 Table
 
+IF OBJECT_ID('Bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE Bronze.erp_loc_a101;
+GO
+
+CREATE TABLE Bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
+
+-- Create Erp_cust_az12 Table
+
+IF OBJECT_ID('Bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE Bronze.erp_cust_az12;
+GO
+
+CREATE TABLE Bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+-- Create Erp_px_cat_g1v2 Table
+
+IF OBJECT_ID('Bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE Bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE Bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
