9d011c3abad6decb88ada887dc39a3c1e5216d0f
subham-buzzed <subhamworks24@gmail.com>
2026-03-31 10:28:38 +0530

    Create CRM tables in Bronze schema
    This SQL script creates three tables: crm_cust_info, crm_prd_info, and crm_sales_details in the Bronze schema. It includes checks to drop existing tables if they exist before creating new ones.

--- Scripts/Bronze/CRM.sql
+++ Scripts/Bronze/CRM.sql
@@ -1,1 +1,59 @@
+-- CREATING BRONZE_CRM_CUST_INFO TABLE
+
+IF OBJECT_ID('Bronze.crm_cust_info','U') IS NOT NULL
+	DROP TABLE Bronze.crm_cust_info;
+GO
+
+CREATE TABLE Bronze.crm_cust_info
+(
+	cst_id				INT,
+	cst_key				NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr			NVARCHAR(50),
+	cst_create_date		DATE
+
+);
+GO
+
+-- CREATING BRONZE_CRM_PRD_INFOTABLE 
+
+IF OBJECT_ID('Bronze.crm_prd_info','U') IS NOT NULL
+	DROP TABLE Bronze.crm_prd_info;
+GO
+
+CREATE TABLE Bronze.crm_prd_info
+(
+	 prd_id       INT,
+     prd_key      NVARCHAR(50),
+     prd_nm       NVARCHAR(50),
+     prd_cost     INT,
+     prd_line     NVARCHAR(50),
+     prd_start_dt DATETIME,
+     prd_end_dt   DATETIME
+
+);
+GO
+
+-- CREATING BRONZE_CRM_SALES_INFO TABLE 
+
+IF OBJECT_ID('Bronze.crm_sales_details','U') IS NOT NULL
+	DROP TABLE Bronze.crm_sales_details;
+GO
+
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
+
+
 
