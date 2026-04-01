1e0a33ee35825b422d609245a5fb1d76f3bde77c
subham-buzzed <subhamworks24@gmail.com>
2026-04-01 14:30:41 +0530

    Refactor CRM data loading stored procedure

--- Scripts/Bronze/SP_Loadscrpit_CRM.sql
+++ Scripts/Bronze/SP_Loadscrpit_CRM.sql
@@ -2,44 +2,71 @@
 
 CREATE OR ALTER PROCEDURE Bronze.load_bronze_crm AS 
 	BEGIN
+		DECLARE @START_TIME DATETIME, @END_TIME DATETIME
+-- ===== LOAD SCRIPT FOR CRM-CUST-INFO =========
+		BEGIN TRY
+			SET @START_TIME = GETDATE();
+				PRINT'Inserting Data info: CRM-cust-info';
 
-	-- ===== LOAD SCRIPT FOR CRM-CUST-INFO =========
+					TRUNCATE TABLE  Bronze.crm_cust_info;
 
-		TRUNCATE TABLE  Bronze.crm_cust_info;
-		
-		BULK INSERT Bronze.crm_cust_info
-		FROM 'C:\DALAIs Document\SQL\DATA\Data Warehouse\sql-data-warehouse-project\datasets\source_crm/cust_info.csv'
-		WITH 
-		(
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+					BULK INSERT Bronze.crm_cust_info
+					FROM 'C:\DALAIs Document\SQL\DATA\Data Warehouse\sql-data-warehouse-project\datasets\source_crm/cust_info.csv'
+					WITH 
+					(
+						FIRSTROW = 2,
+						FIELDTERMINATOR = ',',
+						TABLOCK
+					);
+
+				SET @END_TIME = GETDATE()
+				PRINT'Load time = '+ CAST(DATEDIFF(SECOND,@START_TIME,@END_TIME)AS NVARCHAR)+' seconds'
+				PRINT'======================================================================='
 
 	-- ========== LOAD SCRIPT FOR CRM-PRD-INFO ===========
+				SET @START_TIME = GETDATE();
+					PRINT'Inserting Data info: CRM-prd-info';
 
-		TRUNCATE TABLE  Bronze.crm_prd_info;
+					TRUNCATE TABLE  Bronze.crm_prd_info;
 		
-		BULK INSERT Bronze.crm_prd_info
-		FROM 'C:\DALAIs Document\SQL\DATA\Data Warehouse\sql-data-warehouse-project\datasets\source_crm/prd_info.csv'
-		WITH 
-		(
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+					BULK INSERT Bronze.crm_prd_info
+					FROM 'C:\DALAIs Document\SQL\DATA\Data Warehouse\sql-data-warehouse-project\datasets\source_crm/prd_info.csv'
+					WITH 
+					(
+						FIRSTROW = 2,
+						FIELDTERMINATOR = ',',
+						TABLOCK
+					);
 
-		--============ LOAD SCRIPT FOR CRM-SALES-DETAILS =======
+				SET @END_TIME = GETDATE()
+				PRINT'Load time = '+ CAST(DATEDIFF(SECOND,@START_TIME,@END_TIME) AS NVARCHAR)+' seconds'
+				PRINT'======================================================================='
 
-		TRUNCATE TABLE  Bronze.crm_sales_details;
+
+--============ LOAD SCRIPT FOR CRM-SALES-DETAILS =======
+				SET @START_TIME = GETDATE();
+					PRINT'Inserting Data info: CRM-sale-details';
+
+					TRUNCATE TABLE  Bronze.crm_sales_details;
 		
-		BULK INSERT Bronze.crm_sales_details
-		FROM 'C:\DALAIs Document\SQL\DATA\Data Warehouse\sql-data-warehouse-project\datasets\source_crm/sales_details.csv'
-		WITH 
-		(
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		
+					BULK INSERT Bronze.crm_sales_details
+					FROM 'C:\DALAIs Document\SQL\DATA\Data Warehouse\sql-data-warehouse-project\datasets\source_crm/sales_details.csv'
+					WITH 
+					(
+						FIRSTROW = 2,
+						FIELDTERMINATOR = ',',
+						TABLOCK
+					);
+
+				SET @END_TIME = GETDATE()
+				PRINT'Load time = '+ CAST(DATEDIFF(SECOND,@START_TIME,@END_TIME) AS NVARCHAR)+' seconds'
+				PRINT'======================================================================='
+
+				PRINT 'ALL THE CRM FILES ARE LOADED'
+
+		END TRY
+		BEGIN CATCH
+			PRINT'ERROR OCUURED'
+			PRINT 'ERROR IS:'+ ERROR_MESSAGE()
+		END CATCH
 	END 
