489c38e32127f8c89988e9fa7e4d357244e80429
subham-buzzed <subhamworks24@gmail.com>
2026-04-06 13:51:20 +0530

    Add files via upload

---
+++ Scripts/Silver/crm/DataCleansing_and_Loading_of_crm_cust_info.sql
@@ -0,0 +1,64 @@
+--=====================================================================
+
+/* 
+
+crm_cust_info Files from Bronze layer to Silver Layer after complete testing of Data Quality and Data Completeness
+	where data is:
+					-	DATA WITHOUT ABY DUPLICATE AND NULL cst_ID VALUES
+					-	DATA WITH FIELD HAVING NO BLANK SPACE/ UNWANTED SPACE 
+					-	DATA WITH STANDARDIZATION AND CONSISTENCY
+*/
+--==========================================================================
+IF OBJECT_ID('Silver.crm_cust_info','U') IS NOT NULL 
+	DROP TABLE Silver.crm_cust_info;                    
+GO
+
+CREATE TABLE Silver.crm_cust_info
+(
+	cst_id				INT,
+	cst_key				NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr			NVARCHAR(50),
+	cst_create_date		DATE,
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+
+);
+GO
+
+INSERT INTO Silver.crm_cust_info
+(
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date
+)
+SELECT 
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) cst_firstname,
+	TRIM(cst_lastname) cst_lastname,
+	CASE 
+		WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN  'Married'
+		ELSE 'N/A'
+	END cst_marital_status,
+	CASE 
+		WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+		ELSE 'N/A'
+	END cst_gndr,
+	cst_create_date
+
+FROM 
+	(SELECT
+		*,
+		ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) CHECKS
+	FROM Bronze.crm_cust_info
+		WHERE cst_id IS NOT NULL )T
+ WHERE CHECKS = 1  
+
---
+++ Scripts/Silver/crm/DataCleansing_and_Loading_of_crm_prd_info.sql
@@ -0,0 +1,58 @@
+--=====================================================================
+
+/* 
+
+crm_prd_info Files from Bronze layer to Silver Layer after complete testing of Data Quality and Data Completeness
+	where data is:
+				Possible Data Quality Checks  with crm_prd_info
+					- Has no Data Duplicates
+					- Dealt with Data with unwanted spaces
+					- Dealt with Data with lower case & upper case mismatch
+					- Data Standardization & Consistency is maintained
+					- Data Integration field Checks are done 
+					- Handled Invalid Dates
+*/
+--==========================================================================
+
+IF OBJECT_ID('Silver.crm_prd_info','U') IS NOT NULL
+	DROP TABLE Silver.crm_prd_info;
+GO
+
+CREATE TABLE Silver.crm_prd_info
+(
+	 prd_id       INT,
+	 cat_id       NVARCHAR(50),
+     prd_key      NVARCHAR(50),
+     prd_nm       NVARCHAR(50),
+     prd_cost     INT,
+     prd_line     NVARCHAR(50),
+     prd_start_dt DATETIME,
+     prd_end_dt   DATETIME,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+
+);
+INSERT INTO Silver.crm_prd_info
+(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT 
+	prd_id,
+	REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id,
+	SUBSTRING(prd_key,7,LEN(prd_key)) prd_key,
+	ISNULL(prd_cost,0) prd_cost,
+	CASE TRIM(prd_line)
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'T' THEN 'Touring'
+		WHEN 'S' THEN 'Other Sales'
+		ELSE 'N/A'
+	END prd_line,
+	CAST(prd_start_dt AS DATE) prd_start_dt,
+	CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt ) AS DATE) prd_end_dt
+FROM Bronze.crm_prd_info
