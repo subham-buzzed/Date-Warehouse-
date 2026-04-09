3d43f80076d8b5d71c264cf5b712eaec68cf3f54
subham-buzzed <subhamworks24@gmail.com>
2026-04-09 12:47:37 +0530

    Reformat SQL checks for ERP customer data

--- Scripts/Silver/ERP/Quality_check_erp_cust_az12.sql
+++ Scripts/Silver/ERP/Quality_check_erp_cust_az12.sql
@@ -7,8 +7,10 @@ SELECT COUNT(*) FROM Bronze.erp_cust_az12
 WHERE cid NOT LIKE 'NAS%'
 
 --========= VALID DATE CHECKS
+  
 SELECT * FROM Bronze.erp_cust_az12
 WHERE bdate > GETDATE()
 
 --=== FIELD CHECKS : gen
+  
 SELECT DISTINCT gen FROM Bronze.erp_cust_az12
