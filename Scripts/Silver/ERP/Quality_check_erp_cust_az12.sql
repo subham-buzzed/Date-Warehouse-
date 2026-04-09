38be43511f1133221a98a2f6e18b50f73390423d
subham-buzzed <subhamworks24@gmail.com>
2026-04-09 12:46:53 +0530

    Add files via upload

---
+++ Scripts/Silver/ERP/Quality_check_erp_cust_az12.sql
@@ -0,0 +1,14 @@
+--====== FIELD cid CHCEKS
+
+SELECT COUNT(*) FROM Bronze.erp_cust_az12
+WHERE cid LIKE 'NAS%'
+
+SELECT COUNT(*) FROM Bronze.erp_cust_az12
+WHERE cid NOT LIKE 'NAS%'
+
+--========= VALID DATE CHECKS
+SELECT * FROM Bronze.erp_cust_az12
+WHERE bdate > GETDATE()
+
+--=== FIELD CHECKS : gen
+SELECT DISTINCT gen FROM Bronze.erp_cust_az12
