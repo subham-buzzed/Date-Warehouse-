a79adf0ebbc754039be42330c4bef35f60b133eb
subham-buzzed <subhamworks24@gmail.com>
2026-04-11 06:22:53 +0530

    Create table Silver.erp_px_cat_g1v2
    Drop the existing table if it exists and create a new table with specified columns.

--- Scripts/Silver/ERP/px_cat_g1v2.sql
+++ Scripts/Silver/ERP/px_cat_g1v2.sql
@@ -1,3 +1,15 @@
+IF OBJECT_ID('Silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE Silver.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE Silver.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50),
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
+);
+GO
 INSERT INTO Silver.erp_px_cat_g1v2
 (
 	id,
