b26fd361d773e32e4229dd0a51169e1fd25a2b0e
subham-buzzed <subhamworks24@gmail.com>
2026-04-13 09:31:45 +0530

    Update Checks.sql

--- Scripts/Gold/Checks.sql
+++ Scripts/Gold/Checks.sql
@@ -3,25 +3,12 @@
 -- ============== FIELD: Customer ===========================
 
 --====== DUPLICATE CHECKS ==============
+
 SELECT 
-cst_id,
-COUNT(*)
-FROM
-(SELECT  
-	c1.cst_id,
-	c1.cst_key,
-	c1.cst_firstname,
-	c1.cst_lastname,
-	c1.cst_marital_status,
-	c1.cst_gndr,
-	c2.bdate,
-	c3.cntry
-FROM Silver.crm_cust_info c1
-	LEFT JOIN Silver.erp_cust_az12 c2
-ON c1.cst_key =C2.cid
-	LEFT JOIN Silver.erp_loc_a101 c3
-ON C1.cst_key = c3.cid)T
-GROUP BY cst_id
+	customer_id,
+	COUNT(*)
+FROM Gold.dim_customer
+GROUP BY customer_id 
 HAVING COUNT(*) != 1
 
 --============ DATA INTEGRATION CHECKS (Post integration checks)
@@ -55,28 +42,14 @@ ORDER BY prd_key ,prd_end_dt
 
 
 --===== CHECKING FOR DUPLICATES ==============
+	
 SELECT 
-prd_key
-FROM
-(
-SELECT
-	p1.prd_id,
-	p1.prd_key,
-	p1.cat_id,
-	P2.cat,
-	P2.subcat,
-	p1.prd_nm,
-	p1.prd_line,
-	p1.prd_cost,
-	p2.maintenance,
-	p1.prd_start_dt
-FROM Silver.crm_prd_info p1
-	LEFT JOIN Silver.erp_px_cat_g1v2 p2
-ON p1.cat_id = p2.id
-WHERE p1.prd_end_dt IS NULL)T
-GROUP BY prd_key
-HAVING COUNT(*) >1
-
+	product_id,
+	COUNT(*)
+FROM Gold.dim_product
+GROUP BY product_id
+HAVING COUNT(*) !=1 
+	
 --========== FIELD: SALES(Fact table) ===========================
 
 --========== Foreign key integrity 
