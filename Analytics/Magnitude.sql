fe09148e314ee5fbf8aaea8e6b7f0522f1dde465
subham-buzzed <subhamworks24@gmail.com>
2026-04-15 17:39:15 +0530

    Update Magnitude.sql

--- Analytics/Magnitude.sql
+++ Analytics/Magnitude.sql
@@ -40,3 +40,14 @@ SELECT
 FROM Gold.dim_product
 GROUP BY category
 
+--======== Magnitude oF Total revenue by each customer ========
+
+SELECT 
+	C.customer_key,
+	C.names,
+	SUM(S.sales) Totalsales_by_each_customer
+FROM Gold.fact_sales S LEFT JOIN Gold.dim_customer C
+ON S .customer_key = C.customer_key
+	GROUP BY C.customer_key,C.names
+	ORDER BY Totalsales_by_each_customer DESC
+
