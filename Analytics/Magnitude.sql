ad8c90ae56886ea17796a55afeb7606de112b0d8
subham-buzzed <subhamworks24@gmail.com>
2026-04-15 17:31:41 +0530

    Add SQL queries for customer and product magnitude analysis
    Added SQL queries to analyze customer magnitude by country and gender, as well as product category metrics including total revenue and average price.

---
+++ Analytics/Magnitude.sql
@@ -0,0 +1,42 @@
+--============ Customer Magnitude by gender n country
+-- BY COUNTRY 
+SELECT 
+	C.country,
+	COUNT(DISTINCT C.customer_key) Customer_Per_country 
+FROM GolD.fact_sales S LEFT JOIN Gold.dim_customer C
+	ON S.customer_key = C.customer_key
+GROUP BY C.country
+-- BY GENDER
+SELECT 
+	C.gender,
+	COUNT(DISTINCT C.customer_key) Customer_Per_country 
+FROM GolD.fact_sales S LEFT JOIN Gold.dim_customer C
+	ON S.customer_key = C.customer_key
+GROUP BY C.gender
+-- BY BOTH COUNTRY AND GENDER 
+SELECT 
+	C.country,
+	C.gender,
+	COUNT(DISTINCT C.customer_key) Customer_Per_country 
+FROM GolD.fact_sales S LEFT JOIN Gold.dim_customer C
+	ON S.customer_key = C.customer_key
+GROUP BY C.country,C.gender
+ORDER BY 1,2,3 DESC
+  
+--========= Magnitude of Category BY Total_revenue/total_orders ================
+
+SELECT 
+  P.category,
+  COUNT(DISTINCT S.product_key) Total_Product_by_Category,
+  SUM(S.price) Total_revenue_per_category
+FROM Gold.fact_sales S LEFT JOIN GOLD.dim_product P
+ON S.product_key = P.product_key
+GROUP BY P.category
+
+--======= Magnitude of Category by avg_sales ====
+SELECT 
+	category,
+	AVG(product_cost) Avg_price_per_Category
+FROM Gold.dim_product
+GROUP BY category
+
