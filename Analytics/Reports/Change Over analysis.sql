7867603ba5636b785fc03bcd71a9307eb7535008
subham-buzzed <subhamworks24@gmail.com>
2026-04-20 14:01:34 +0530

    Add Change Over analysis report for sales insights
    Added a report for generating insights on yearly and monthly sales changes, including total sales, quantity, and customer count.

--- Analytics/Reports/Change Over analysis.sql
+++ Analytics/Reports/Change Over analysis.sql
@@ -1,1 +1,45 @@
+/**
+  
+  THIS REPORTS  HELPS TO GENERATE INSIGHTS ABOUT
+                        - Yealy Sales <-> C1
+                        - Total sales Change in each month in each year <-> C2
 
+THAT CAN BE USED FOR EFFECTIVE ANALYTICAL DASHBOARD / ADVANCE ANALYSIS
+
+========================================================================================
+      ** JUST TO USE WHERE CLAUSE FOR SPECIFIC TIMESPAN **
+*/
+
+--========== CHANGE OVER YEARS ============
+-- C1
+SELECT 
+	DATENAME(YEAR,order_date) Years,
+	SUM(sales) Total_sales,
+	SUM(quantity) Total_quantity,
+	COUNT(DISTINCT customer_key) Total_customer
+FROM GOLD.fact_sales
+WHERE order_date IS NOT NULL
+	GROUP BY DATENAME(YEAR,order_date)
+	ORDER BY DATENAME(YEAR,order_date)
+
+--=========== CHANGE OVER MONTHS ========
+--== Targets the months that has highest sales and customers =====
+  -- C2
+
+SELECT 
+	FORMAT(Dates,'yyyy-MMM') Dates,
+	Total_Sales,
+	Total_quantity,
+	Total_customer
+FROM 
+(
+	SELECT 
+	DATETRUNC(MONTH,order_date) Dates,
+	SUM(sales) Total_Sales,
+	SUM(quantity) Total_quantity,
+	COUNT(DISTINCT customer_key) Total_customer
+FROM Gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(MONTH,order_date)
+)T
+--WHERE YEAR(Dates) = 
