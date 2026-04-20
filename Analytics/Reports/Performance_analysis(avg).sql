26c60a6fad7f87f9a626b275c103f03e3b88c50b
subham-buzzed <subhamworks24@gmail.com>
2026-04-20 19:58:25 +0530

    Refactor AVG calculation in performance analysis query

--- Analytics/Reports/Performance_analysis(avg).sql
+++ Analytics/Reports/Performance_analysis(avg).sql
@@ -17,12 +17,12 @@ WITH Yearly_product_sales AS
 SELECT 
 	product_name,
 	Dates,
-	AVG(Total_sales) OVER(PARTITION BY product_name ORDER BY Dates) Avg_sales,
+	AVG(Total_sales) OVER(PARTITION BY product_name) Avg_sales,
 	Total_sales,
-	Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name ORDER BY Dates) AS Diff,
+	Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name) AS Diff,
 	CASE 
-		WHEN Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name ORDER BY Dates) > 0 THEN 'Above avg'
-		WHEN Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name ORDER BY Dates) < 0 THEN 'Below avg'
+		WHEN Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name) > 0 THEN 'Above avg'
+		WHEN Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name) < 0 THEN 'Below avg'
 		ELSE 'Avg'
 	END Product_status
 FROM Yearly_product_sales
