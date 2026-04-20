9ca8d73bfaddc2ae88e84b6c3add7f45deba7411
subham-buzzed <subhamworks24@gmail.com>
2026-04-20 19:48:22 +0530

    Add SQL script for product performance analysis report

---
+++ Analytics/Reports/Performance_analysis(avg).sql
@@ -0,0 +1,28 @@
+--=========== Product PerFormance Analysis =========
+ --- METRIC (AVG SALES IN THAT YEAR)
+WITH Yearly_product_sales AS
+(
+	SELECT 
+		P.product_name,
+		YEAR(order_date) Dates,
+		SUM(S.sales) Total_sales
+	FROM GOLD.fact_sales S
+	LEFT JOIN Gold.dim_product P
+	ON S.product_key = P.product_key
+		WHERE S.order_date IS NOT NULL
+	GROUP BY P.product_name , YEAR(order_date) 
+	
+)
+
+SELECT 
+	product_name,
+	Dates,
+	AVG(Total_sales) OVER(PARTITION BY product_name ORDER BY Dates) Avg_sales,
+	Total_sales,
+	Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name ORDER BY Dates) AS Diff,
+	CASE 
+		WHEN Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name ORDER BY Dates) > 0 THEN 'Above avg'
+		WHEN Total_sales - 	AVG(Total_sales) OVER(PARTITION BY product_name ORDER BY Dates) < 0 THEN 'Below avg'
+		ELSE 'Avg'
+	END Product_status
+FROM Yearly_product_sales
